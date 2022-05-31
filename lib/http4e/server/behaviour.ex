c15696f417a2a4c7b892926ccb2928242d56cf80
Alexander Afanasyev <alexander.afanasyev@aviasales.ru>
2022-05-31 22:03:40 +0700

    Introduce HTTP web server abstraction

---
+++ lib/http4e/server/behaviour.ex
@@ -0,0 +1,6 @@
+defmodule Http4e.Server.Behaviour do
+  @callback start(
+              handler: Http4e.Handler.Behaviour.t(),
+              listen_port: non_neg_integer()
+            ) :: {:ok, shutdown_server :: (-> :ok)}
+end
